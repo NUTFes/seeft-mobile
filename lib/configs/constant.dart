a24755fee9189ad4b672df43b19f658f53b27a3c
YushiroDodo63 <yushiro0603@gmail.com>
2021-09-20 00:33:00 +0900

    [fix]apiUrlの変更

--- lib/configs/constant.dart
+++ lib/configs/constant.dart
@@ -2,5 +2,5 @@ Constant constant = Constant();
 
 class Constant {
   final String appName = "SeeFT";
-  final String apiUrl = "http://localhost:3000";
+  final String apiUrl = "http://20.63.185.41:3000/";
 }
