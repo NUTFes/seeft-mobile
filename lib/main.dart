94c9b44ba19e2e6bf15e44be8f2f9fbb188563a6
Mashimo <toshichika-mashimo@retty.me>
2022-07-10 11:29:08 +0900

    create hooks provider example

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final helloWorldProvider = Provider((_) => 'Hello world');
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('HooksRiverpodExample')),
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
+  }
+}
